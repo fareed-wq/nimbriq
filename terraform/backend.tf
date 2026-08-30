72e409aa80c556b66f712dbec430d2e6c5a7d15b
fareed-wq <fareed2official@gmail.com>
2026-08-31 02:03:31 +0300

    refactor: remove hardcoded AWS profile

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket       = "nimbriq-tfstate-197907147885-euc1"
     key          = "nimbriq/terraform.tfstate"
     region       = "eu-central-1"
-    profile      = "nimbriq"
     encrypt      = true
     use_lockfile = true
   }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-  region  = var.aws_region
-  profile = "nimbriq"
+  region = var.aws_region
 
   default_tags {
     tags = {
